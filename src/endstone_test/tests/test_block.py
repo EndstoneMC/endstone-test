715c2f9d33994b809696eeab3358553c30bf128f
Vincent <magicdroidx@gmail.com>
2024-09-02 14:18:55 +0100

    feat: add test for creating block data with default states

--- src/endstone_test/tests/test_block.py
+++ src/endstone_test/tests/test_block.py
@@ -8,7 +8,15 @@ def server(plugin: Plugin) -> Server:
     return plugin.server
 
 
-def test_create_block_data(server: Server) -> None:
+def test_create_block_data_default_block_states(server: Server) -> None:
+    block_data = server.create_block_data("minecraft:standing_sign")
+    assert block_data.type == "minecraft:standing_sign"
+    assert "ground_sign_direction" in block_data.block_states
+    assert block_data.block_states["ground_sign_direction"] == 0
+    assert "block_light_level" not in block_data.block_states
+
+
+def test_create_block_data_custom_block_states(server: Server) -> None:
     block_data = server.create_block_data("minecraft:standing_sign", {"ground_sign_direction": 8})
     assert block_data.type == "minecraft:standing_sign"
     assert "ground_sign_direction" in block_data.block_states
