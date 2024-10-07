21f641f6d0607fd353ac4e3d98dd47aaefb40698
Vincent <magicdroidx@gmail.com>
2024-10-07 21:47:52 +0100

    refactor: move player command tests into a separate file

---
+++ src/endstone_test/tests/on_player_join/test_command.py
@@ -0,0 +1,14 @@
+
+from endstone import Player, Server
+import pytest
+
+
+@pytest.fixture
+def server(player: Player) -> Server:
+    return player.server
+
+def test_run_command(player: Player):
+    assert player.perform_command("test sender")
+
+def test_run_command_as_server(player: Player, server: Server):
+    server.dispatch_command(server.command_sender, f'execute as "{player.name}" run scriptevent endstone:test Hello World!')
