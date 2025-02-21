79e957967dde8e8bdf2055f1c89f3e7fb0e71748
Vincent <magicdroidx@gmail.com>
2025-02-21 12:17:19 +0000

    feat: add tests for retrieving highest block in a dimension

--- src/endstone_test/tests/on_player_join/test_block.py
+++ src/endstone_test/tests/on_player_join/test_block.py
@@ -19,3 +19,21 @@ def test_get_block_at_location(player: Player) -> None:
     location = player.location
     block = player.dimension.get_block_at(location)
     assert block.data.type == block.type
+
+
+def test_get_highest_block_y_at(player: Player) -> None:
+    location = player.location
+    y = player.dimension.get_highest_block_y_at(location.block_x, location.block_z)
+    assert y != 0
+
+
+def test_get_highest_block_at_xz(player: Player) -> None:
+    location = player.location
+    block = player.dimension.get_highest_block_at(location.block_x, location.block_z)
+    assert block.type != "minecraft:air"
+
+
+def test_get_highest_block_at_location(player: Player) -> None:
+    location = player.location
+    block = player.dimension.get_highest_block_at(location)
+    assert block.type != "minecraft:air"
