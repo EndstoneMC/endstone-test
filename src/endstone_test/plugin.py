c8f16c67247ae413d03fe6334b77f8d0aa7c23df
Vincent <magicdroidx@gmail.com>
2025-03-17 23:00:15 +0000

    chore: bump api version to 0.6

--- src/endstone_test/plugin.py
+++ src/endstone_test/plugin.py
@@ -7,7 +7,7 @@ from endstone_test.test_helper import FixtureInjection, run_tests
 
 class EndstoneTest(Plugin):
     prefix = "EndstoneTest"
-    api_version = "0.5"
+    api_version = "0.6"
     load = PluginLoadOrder.POSTWORLD
 
     commands = {
