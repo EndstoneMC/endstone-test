e54ef77904f090ee2063c608851a0ccad95e61f4
Vincent <magicdroidx@gmail.com>
2025-04-12 18:44:10 +0800

    refactor: remove redundant codes

--- src/endstone_test/plugin.py
+++ src/endstone_test/plugin.py
@@ -42,9 +42,3 @@ class EndstoneTest(Plugin):
 
     def on_disable(self) -> None:
         self.logger.info("on_disable is called!")
-
-    def run_tests(self) -> None:
-        ret_code = pytest.main(
-            ["-s", "--pyargs", "endstone_test.tests"], plugins=[FixtureInjection(self)]
-        )
-        self.logger.info(f"Testing finished with exit code {ret_code}")
