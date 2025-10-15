b1302711a1631b008ae86285d80fe861d4a39105
Vincent <magicdroidx@gmail.com>
2025-10-15 20:57:14 +0100

    fix: remove debug print and initialize MapRenderer with is_contextual flag

--- src/endstone_test/image_renderer.py
+++ src/endstone_test/image_renderer.py
@@ -6,10 +6,9 @@ from PIL.Image import Image, Resampling
 
 class ImageRenderer(MapRenderer):
     def __init__(self, image: Image):
-        MapRenderer.__init__(self)
+        MapRenderer.__init__(self, is_contextual=False)
         self.image = image.convert("RGBA")
         self.image.thumbnail((128, 128), Resampling.LANCZOS)
 
     def render(self, view: MapView, canvas: MapCanvas, player: Player) -> None:
-        print("render!!!")
         canvas.draw_image(0, 0, np.array(self.image))
